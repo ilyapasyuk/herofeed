f2df1cdeba66316031c68d7a0c4950af8a760ee2
ilyapasyuk <pasyuk.com@gmail.com>
2017-09-17 20:25:04 +0300

    review fixes

--- src/hero/hero.service.js
+++ src/hero/hero.service.js
@@ -7,13 +7,6 @@ export default ($http) => {
             return $http.get(idUrl).then((response) => {
                 return Object.values(response.data);
             });
-        },
-        getHeros() {
-            const herosUrl = 'https://api.airtable.com/v0/app0a8OYcOZAv6uCv/characters?api_key=keyFR1R9B9wqDZeOz';
-
-            return $http.get(herosUrl).then((response) => {
-                return Object.values(response.data);
-            });
         }
     };
 };
