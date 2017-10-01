2a58c3fdd2b8f99043c28ec6a068b21a57ed50cd
Ilya Pasuyk <pasukilya@gmail.com>
2017-10-01 22:18:29 +0300

    add reset button & show current month, year in filter (#15)

--- src/movies/movie-list/movie-list.component.js
+++ src/movies/movie-list/movie-list.component.js
@@ -20,15 +20,24 @@ function MovieListController() {
 
     vm.$onInit = () => {
         vm.data = angular.copy(vm.list);
+        vm.thisYear = Moment().year();
+        vm.thisMonth = Moment().format('MMMM');
     };
 
     vm.getThisYearMovies = () => {
+        vm.activateFilters = true;
         const thisYear = Moment().startOf('year').format('YYYY');
         vm.data = vm.list.filter((movie) => movie.fields.date_realise.slice(0, -6) === thisYear);
     };
 
     vm.getThisMonthMovies = () => {
+        vm.activateFilters = true;
         const thisMonth = Moment().startOf('month').format('YYYY-MM');
         vm.data = vm.list.filter((movie) => movie.fields.date_realise.slice(0, -3) === thisMonth);
+    };
+
+    vm.resetFilters = () => {
+        vm.activateFilters = false;
+        vm.data = vm.list;
     };
 }
