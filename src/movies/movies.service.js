19c154fb2e8782836708fde87cbf2cb938f63ab1
ilyapasyuk <pasyuk.com@gmail.com>
2017-10-01 15:43:26 +0300

    add filter by month & year

--- src/movies/movies.service.js
+++ src/movies/movies.service.js
@@ -6,7 +6,7 @@ export default function($http) {
             const movieListUrl = 'https://api.airtable.com/v0/app0a8OYcOZAv6uCv/movies?api_key=keyFR1R9B9wqDZeOz&sort%5B0%5D%5Bfield%5D=date_realise&sort%5B0%5D%5Bdirection%5D=desc';
 
             return $http.get(movieListUrl).then((response) => {
-                return Object.values(response.data);
+                return Object.values(response.data.records);
             });
         },
         getMovie(idMovie) {
