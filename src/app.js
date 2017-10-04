e9bd1ec96d837c96fbddd8f56a89514c53ea97af
ilyapasyuk <pasyuk.com@gmail.com>
2017-10-04 22:20:31 +0300

    add blog

--- src/app.js
+++ src/app.js
@@ -3,12 +3,14 @@ import CoreModule from './core/core.module';
 import MoviesModule from './movies/movies.module';
 import GamesModule from './games/games.module';
 import HeroModule from './hero/hero.module';
+import NewsModule from './news/news.module';
 
 angular.module('app', [
     CoreModule,
     MoviesModule,
     GamesModule,
-    HeroModule
+    HeroModule,
+    NewsModule
 ]).config(($compileProvider) => {
     $compileProvider.debugInfoEnabled(process.env.NODE_ENV !== 'production');
     $compileProvider.commentDirectivesEnabled(false);
