18db130d9d20ed5d233a17971291e7239c60e9e0
ilyapasyuk <pasyuk.com@gmail.com>
2017-12-27 01:36:22 +0300

    delete angular

--- src/core/app/app.component.js
+++
@@ -1,6 +0,0 @@
-import template from './app.html';
-import './app.scss';
-
-export default {
-    template
-};
--- src/core/menu/menu.directive.js
+++
@@ -1,6 +0,0 @@
-import Menu from './Menu.jsx';
-
-const ReactMenu = (reactDirective) => reactDirective(Menu);
-ReactMenu.$inject = ['reactDirective'];
-
-export default ReactMenu;
