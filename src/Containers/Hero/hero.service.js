eb5af31c29e5dbf7b0acfbe836254ade2a3c1d46
ilyapasyuk <pasyuk.com@gmail.com>
2018-02-14 23:15:12 +0300

    add ESDoc

--- src/Containers/Hero/hero.service.js
+++ src/Containers/Hero/hero.service.js
@@ -1,10 +1,19 @@
 import axios from 'axios';
 
+/**
+ * HeroService stores the methods for obtaining data about the superheros
+ * @example
+ * const Hero = new HeroService();
+ */
 export default class HeroService {
+    /**
+     * @return {Object} with hero information.
+     */
     getHero(idHero) {
+        /**
+         * @type {string}
+         */
         const idUrl = `https://api.airtable.com/v0/app0a8OYcOZAv6uCv/characters?api_key=keyFR1R9B9wqDZeOz&filterByFormula=id="${idHero}"`;
-        return axios.get(idUrl).then((response) => {
-            return response.data.records[0].fields;
-        });
+        return axios.get(idUrl).then((response) => response.data.records[0].fields);
     }
-};
+}
--- src/Containers/Movies/movies.service.js
+++ src/Containers/Movies/movies.service.js
@@ -1,8 +1,19 @@
 import axios from 'axios';
 import Qs from 'qs';
 
+/**
+ * MoviesService stores the methods for obtaining data about the movies
+ * @example
+ * const Movies = new MoviesService();
+ */
 export default class MoviesService {
+    /**
+     * @return {Object} with movie list.
+     */
     getList() {
+        /**
+         * @type {Object}
+         */
         const params = {
             api_key: 'keyFR1R9B9wqDZeOz',
             sort: [{
@@ -11,6 +22,9 @@ export default class MoviesService {
             }],
         };
 
+        /**
+         * @type {String}
+         */
         const movieListUrl = 'https://api.airtable.com/v0/app0a8OYcOZAv6uCv/Movies';
 
         return axios({
@@ -22,7 +36,13 @@ export default class MoviesService {
             .then((response) => response.data.records);
     }
 
+    /**
+     * @return {Object} with movie info.
+     */
     getMovie(idMovie) {
+        /**
+         * @type {String}
+         */
         const idMovieUrl = `https://api.airtable.com/v0/app0a8OYcOZAv6uCv/movies?api_key=keyFR1R9B9wqDZeOz&filterByFormula=id="${idMovie}"`;
 
         return axios.get(idMovieUrl)
