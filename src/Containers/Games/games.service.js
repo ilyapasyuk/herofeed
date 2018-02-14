aed21cf779abd642b3b1b062b6fb15133063b504
Ilya Pasuyk <pasukilya@gmail.com>
2018-02-14 23:19:32 +0300

    add ESDoc (#62)

--- src/Containers/Games/games.service.js
+++ src/Containers/Games/games.service.js
@@ -1,8 +1,20 @@
 import axios from 'axios';
 
+/**
+ * @type {string}
+*/
 const SOURCE = 'https://api.airtable.com/v0/app0a8OYcOZAv6uCv/Games?api_key=keyFR1R9B9wqDZeOz&sort%5B0%5D%5Bfield%5D=date_relise&sort%5B0%5D%5Bdirection%5D=desc';
 
+/**
+ * GamesService stores the methods for obtaining data about the games
+ * @example
+ * const Games = new GamesService();
+ */
 export default class GamesService {
+
+    /**
+     * @return {Object} with games list.
+     */
     getList() {
         return axios.get(SOURCE).then((response) => {
             return Object.values(response.data.records);
