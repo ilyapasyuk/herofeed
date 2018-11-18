6a040e60cf0a6af898d1d3bc52d136af4174efb0
ilyapasyuk <pasyuk.com@gmail.com>
2018-11-19 01:14:29 +0300

    yarn & prettier

--- src/Containers/Games/GameList/index.jsx
+++ src/Containers/Games/GameList/index.jsx
@@ -1,40 +1,35 @@
-import React, {Component} from 'react';
-import GameItem from '../GameItem';
-import './style.scss';
-import GamesService from '../games.service';
+import React, { Component } from 'react'
+import GameItem from '../GameItem'
+import './style.scss'
+import GamesService from '../games.service'
 
-const Games = new GamesService();
+const Games = new GamesService()
 
 export default class GameList extends Component {
     constructor() {
-        super();
+        super()
         this.state = {
             items: [],
-        };
+        }
     }
 
     componentDidMount() {
-        Games.getList()
-            .then((response) => {
-                this.setState({
-                    items: response,
-                });
-            });
+        Games.getList().then((response) => {
+            this.setState({
+                items: response,
+            })
+        })
     }
 
     render() {
         return (
             <div className="Games-list">
                 {this.state.items.map((item) => (
-                    <div
-                        className="col-sm-3"
-                        key={item.id}
-                    >
+                    <div className="col-sm-3" key={item.id}>
                         <GameItem data={item.fields} />
                     </div>
                 ))}
             </div>
-        );
+        )
     }
 }
-
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {HashRouter} from 'react-router-dom';
-import App from './Containers/App';
-import './UI/styles/core.scss';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { HashRouter } from 'react-router-dom'
+import App from './Containers/App'
+import './UI/styles/core.scss'
 
 ReactDOM.render(
     <HashRouter>
         <App />
-    </HashRouter>, document.getElementById('app')
-);
+    </HashRouter>,
+    document.getElementById('app'),
+)
