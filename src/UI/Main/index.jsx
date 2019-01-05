0febd21a6a25453b206d8e17534fc8133330feed
ilyapasyuk <pasyuk.com@gmail.com>
2019-01-05 23:18:07 +0300

    add React Lazy

--- src/UI/Main/index.jsx
+++ src/UI/Main/index.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { Switch, Route } from 'react-router-dom'
-import MovieList from '../../Containers/Movies/MovieList'
+// import MovieList from '../../Containers/Movies/MovieList'
+const MovieList = React.lazy(() => import('../../Containers/Movies/MovieList'))
 import MoviePage from '../../Containers/Movies/MoviePage'
 import GameList from '../../Containers/Games/GameList'
 import './style.scss'
@@ -8,7 +9,9 @@ import './style.scss'
 const Main = () => (
     <main className="container">
         <Switch>
-            <Route exact path="/" component={MovieList} />
+            <Suspense fallback={<div>Loading...</div>}>
+                <Route exact path="/" component={MovieList} />
+            </Suspense>
             <Route exact path="/movies" component={MovieList} />
             <Route path="/games" component={GameList} />
             <Route path="/movie/:id" component={MoviePage} />
