956c5fef3602d63aa2852ff596cb123c8131aaa5
ilyapasyuk <pasyuk.com@gmail.com>
2019-04-28 14:48:07 +0300

    add async/await

--- src/Containers/Games/GameList/index.jsx
+++ src/Containers/Games/GameList/index.jsx
@@ -1,27 +1,26 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
 import GameItem from '../GameItem'
 import GamesService from 'Services/Games'
 
-class GameList extends Component {
-    constructor() {
-        super()
-        this.state = {
-            items: [],
-        }
+class GameList extends PureComponent {
+    state = {
+        games: [],
     }
 
-    componentDidMount() {
-        GamesService.getList().then((response) => {
-            this.setState({
-                items: response,
-            })
+    async componentDidMount() {
+        const games = await GamesService.getList()
+
+        this.setState({
+            games,
         })
     }
 
     render() {
+        const { games } = this.state
+
         return (
             <div className="Games-list row">
-                {this.state.items.map((item) => (
+                {games.map((item) => (
                     <div className="col-sm-3" key={item.id}>
                         <GameItem data={item.fields} />
                     </div>
