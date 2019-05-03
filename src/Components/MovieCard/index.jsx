cb6916456283dd057bf279999df0254f9eaa06ad
ilyapasyuk <pasyuk.com@gmail.com>
2019-05-03 17:14:42 +0300

    delete preloader

--- src/Components/MovieCard/index.jsx
+++ src/Components/MovieCard/index.jsx
@@ -6,14 +6,7 @@ import Dayjs from 'dayjs'
 const MovieCard = ({ type, title, cover, id, realise }) => (
     <Link className="Movie-item" to={`/movie/${id}`} alt={title}>
         <div className="Movie-item__poster">
-            {cover !== '' && (
-                <div id="preloader">
-                    <div id="loader" />
-                </div>
-            )}
-
             <span className={`Movie-item__type Movie-item__type_${type}`}>{type}</span>
-
             <span className="Movie-item__date-realise">{Dayjs(realise).format('DD.MM.YYYY')}</span>
             {cover && <img src={cover} alt={title} />}
         </div>
