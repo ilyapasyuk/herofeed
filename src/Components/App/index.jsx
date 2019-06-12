39f70e23f5cfed8a718af7bdd3e368b49c6f48b4
ilyapasyuk <pasyuk.com@gmail.com>
2019-06-12 14:27:02 +0300

    add jss

--- src/Components/App/index.jsx
+++ src/Components/App/index.jsx
@@ -1,5 +1,6 @@
 import React, { Suspense } from 'react'
 import { Switch, Route } from 'react-router-dom'
+import { createUseStyles } from 'react-jss'
 
 // Components
 import Menu from 'Components/Menu'
@@ -13,11 +14,18 @@ const MovieList = React.lazy(() => import('../../Containers/MoviesList'))
 const MoviePage = React.lazy(() => import('../../Containers/MoviePage'))
 const GameList = React.lazy(() => import('../../Containers/GameList'))
 
+const useStyles = createUseStyles({
+    container: {
+        paddingTop: 120,
+    },
+})
+
 function App() {
+    const styles = useStyles()
     return (
         <Suspense fallback={<SplashScreen />}>
             <Menu />
-            <main className="App_container container">
+            <main className={styles.container}>
                 <Switch>
                     <Route exact path="/" component={MovieList} />
                     <Route path={ROUTES.MOVIES.LIST} component={MovieList} />
