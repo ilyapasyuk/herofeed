3d1de2d4ceb7d1aa4d01513f2de6a715481b3e68
ilyapasyuk <pasyuk.com@gmail.com>
2019-06-12 16:29:31 +0300

    delete .scss files

--- src/Components/Button/index.jsx
+++ src/Components/Button/index.jsx
@@ -1,18 +1,48 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import ClassNames from 'classnames'
+import { createUseStyles } from 'react-jss'
 
-import './styles.scss'
+const useStyles = createUseStyles({
+    button: isPrimary => {
+        const buttonStyle = {
+            display: 'inline-block',
+            cursor: 'pointer',
+            fontSize: '14px',
+            padding: '0.75rem 2rem',
+            fontWeight: 400,
+            textAlign: 'center',
+            whiteSpace: 'nowrap',
+            verticalAlign: 'middle',
+            userSelect: 'none',
+            borderRadius: '0.25rem',
+            transition: 'background-color 0.1s linear 0s',
+            outline: 'none',
+            color: '#212529',
+            background: 'white',
+            boxShadow: '0px 3px 20px 0px rgba(113, 106, 202, 0.17) !important',
+            border: '1px solid #ffffff',
+        }
 
-const Button = ({ title, callBackClick, isPrimary, isLink, isBlock, elementId }) => {
-    const style = ClassNames('Button', {
-        Button_primary: isPrimary,
-        Button_link: isLink,
-        Button_block: isBlock,
-    })
+        if (isPrimary) {
+            return {
+                ...buttonStyle,
+                ...{
+                    backgroundColor: '#5867dd',
+                    boxShadow: '0px 5px 10px 2px rgba(88, 103, 221, 0.19) !important',
+                    border: '1px solid #5867dd',
+                    color: '#fff',
+                },
+            }
+        }
 
+        return buttonStyle
+    },
+})
+
+const Button = ({ title, callBackClick, isPrimary, elementId }) => {
+    const styles = useStyles(isPrimary)
     return (
-        <button id={elementId} className={style} onClick={callBackClick} type="button">
+        <button id={elementId} className={styles.button} onClick={callBackClick} type="button">
             {title}
         </button>
     )
@@ -22,15 +52,11 @@ Button.propTypes = {
     title: PropTypes.string.isRequired,
     callBackClick: PropTypes.func.isRequired,
     elementId: PropTypes.string.isRequired,
-    isLink: PropTypes.bool,
     isPrimary: PropTypes.bool,
-    isBlock: PropTypes.bool,
 }
 
 Button.defaultProps = {
     isPrimary: false,
-    isLink: false,
-    isBlock: false,
 }
 
 export default Button
