6b36e4b4d52abe58631a6322ef03814172416298
ilyapasyuk <pasyuk.com@gmail.com>
2019-11-09 19:34:31 +0300

    styled components

--- src/Components/Menu/index.jsx
+++ src/Components/Menu/index.jsx
@@ -1,76 +1,56 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
-import { createUseStyles } from 'react-jss'
+import styled from 'styled-components'
 
 import PAGES from 'Constants/menu'
+import logoImage from './logo.png'
 
-import Logo from './logo.png'
+const Menu = styled.div`
+    padding: 0;
+    background: #262734;
+    height: 70px;
+    backface-visibility: hidden;
+    transition: all 0.3s ease;
+    box-shadow: 0 1px 15px 1px rgba(81, 77, 92, 0.5);
+    position: fixed;
+    width: 100%;
+    z-index: 3;
+    display: flex;
+`
 
-const useStyles = createUseStyles({
-    base: {
-        padding: 0,
-        background: '#262734',
-        height: 70,
-        backfaceVisibility: 'hidden',
-        transition: 'all 0.3s ease',
-        boxShadow: '0px 1px 15px 1px rgba(81, 77, 92, 0.5)',
-        position: 'fixed',
-        width: '100%',
-        zIndex: 3,
-    },
-    container: {
-        display: 'flex',
-        height: '100%',
-    },
-    logo: {
-        height: '100%',
-    },
-    items: {
-        display: 'flex',
-        height: '100%',
-    },
-    item: {
-        color: '#6c6e86',
-        textDecoration: 'none',
-        padding: '0px 20px',
-        height: '100%',
-        alignItems: 'center',
-        display: 'flex',
-        transition: 'all 0.3s ease',
-        '&:hover': {
-            color: '#fff',
-        },
-        '&:active': {
-            color: '#fff',
-        },
-    },
-})
+const Logo = styled(Link)`
+    img {
+        height: 100%;
+    }
+`
 
-function Menu() {
-    const classes = useStyles()
+const MenuItem = styled(NavLink)`
+    color: #6c6e86;
+    text-decoration: none;
+    padding: 0 20px;
+    height: 100%;
+    align-items: center;
+    display: inline-flex;
+    transition: all 0.3s ease;
+    &:hover {
+        color: #fff;
+    }
 
-    return (
-        <div className={classes.base}>
-            <div className={classes.container}>
-                <Link to="/" className={classes.logo}>
-                    <img src={Logo} alt="Herofeed" className={classes.logo} />
-                </Link>
+    &:active {
+        color: #fff;
+    }
+`
 
-                <div className={classes.items}>
-                    {PAGES.map(page => (
-                        <NavLink
-                            to={page.state}
-                            className={classes.item}
-                            activeClassName="Menu__item_active"
-                            key={page.state}
-                        >
-                            {page.name}
-                        </NavLink>
-                    ))}
-                </div>
-            </div>
-        </div>
-    )
-}
+export default () => (
+    <Menu>
+        <Logo to="/">
+            <img src={logoImage} alt="Herofeed" />
+        </Logo>
 
-export default Menu
+        {PAGES.map(page => (
+            <MenuItem to={page.state} activeClassName="Menu__item_active" key={page.state}>
+                {page.name}
+            </MenuItem>
+        ))}
+    </Menu>
+)
