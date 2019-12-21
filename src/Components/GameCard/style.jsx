dc43d4a8a09f61e6a61eab9a4081725b56292faf
Ilya Pasuyk <pasyuk.com@gmail.com>
2019-12-21 15:23:55 +0300

    update libs (#423)

---
+++ src/Components/GameCard/style.jsx
@@ -0,0 +1,58 @@
+import styled from 'styled-components'
+import variables from 'Components/styles/variables'
+
+const Card = styled.div`
+    display: block;
+    padding: 0;
+    background: white;
+    position: relative;
+    border-radius: ${variables.baseSize}px;
+    margin-bottom: 20px;
+    border: 1px solid #e6e6e6;
+    width: 100%;
+    overflow: hidden;
+`
+
+const Title = styled.h4`
+    padding-top: ${variables.baseSize * 3}px;
+    padding-bottom: ${variables.baseSize * 3}px;
+    padding-left: ${variables.baseSize * 4}px;
+    padding-right: ${variables.baseSize * 4}px;
+    font-size: 14px;
+    margin: 0;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    border-bottom: 1px solid #e6e6e6;
+`
+
+const Realise = styled.div`
+    padding-top: ${variables.baseSize * 3}px;
+    padding-bottom: ${variables.baseSize * 3}px;
+    padding-left: ${variables.baseSize * 4}px;
+    padding-right: ${variables.baseSize * 4}px;
+    font-size: 12px;
+    opacity: 0.4;
+`
+
+const Platforms = styled.div`
+    padding-top: ${variables.baseSize * 3}px;
+    padding-bottom: ${variables.baseSize * 3}px;
+    padding-left: ${variables.baseSize * 4}px;
+    padding-right: ${variables.baseSize * 4}px;
+    font-size: 12px;
+    display: flex;
+    overflow-x: auto;
+`
+
+const Platform = styled.div`
+    background-color: ${variables.completeLighter};
+    border-radius: ${variables.baseSize}px;
+    white-space: nowrap;
+    padding-top: ${variables.baseSize}px;
+    padding-bottom: ${variables.baseSize}px;
+    padding-left: ${variables.baseSize * 2}px;
+    padding-right: ${variables.baseSize * 2}px;
+    margin-right: ${variables.baseSize * 2}px;
+`
+export { Card, Realise, Title, Platform, Platforms }
--- src/Components/Menu/index.jsx
+++ src/Components/Menu/index.jsx
@@ -1,77 +1,16 @@
 import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
-import styled from 'styled-components'
-import { Wizard } from 'react-onboarding'
-
 import PAGES from 'Constants/menu'
-import logoImage from './logo.png'
 
-const rule = [
-    {
-        elementId: 'logo',
-        title: 'Title 1',
-        description: 'description 1',
-    },
-    {
-        elementId: 'menu',
-        title: 'Title 2',
-        description: 'description 2',
-    },
-]
+import { StyledHeader, StyledItem } from './style'
 
-const Menu = styled.div`
-    padding: 0;
-    background: #262734;
-    height: 70px;
-    backface-visibility: hidden;
-    transition: all 0.3s ease;
-    box-shadow: 0 1px 15px 1px rgba(81, 77, 92, 0.5);
-    position: fixed;
-    width: 100%;
-    z-index: 3;
-    display: flex;
-`
-
-const Logo = styled(Link)`
-    display: inline-flex;
-    flex: 0;
-
-    img {
-        height: 100%;
-        display: flex;
-    }
-`
-
-const MenuItem = styled(NavLink)`
-    color: #6c6e86;
-    text-decoration: none;
-    padding: 0 20px;
-    height: 100%;
-    align-items: center;
-    display: inline-flex;
-    transition: all 0.3s ease;
-    &:hover {
-        color: #fff;
-    }
-
-    &:active {
-        color: #fff;
-    }
-`
-
-export default () => (
-    <Menu>
-        <Wizard rule={rule} />
-        <Logo to="/" id="logo">
-            <img src={logoImage} alt="Herofeed" />
-        </Logo>
-
-        <div id="menu">
-            {PAGES.map(page => (
-                <MenuItem to={page.state} activeClassName="Menu__item_active" key={page.state}>
-                    {page.name}
-                </MenuItem>
-            ))}
-        </div>
-    </Menu>
+const Header = () => (
+    <StyledHeader>
+        {PAGES.map(page => (
+            <StyledItem to={page.state} activeClassName="Header__item_active" key={page.state}>
+                {page.name}
+            </StyledItem>
+        ))}
+    </StyledHeader>
 )
+
+export default Header
